0f81e50ec5feb62e667a634765ba35eb32e16ea3
Eligv99 <eliezer.vazquez99@gmail.com>
2019-04-18 18:23:40 -0400

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Navbar from "./components/Navbar"
+import Header from "./components/Header"
+import Container from "./components/Container"
+import Footer from "./components/Footer"
+
+function App() {
+
+  return(
+    <div>
+      <Navbar/>
+      <Header/>
+      <Container/>
+      <Footer/>
+
+
+    </div>
+  );
+  
+}
+
+export default App;
